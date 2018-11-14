b8e149632ab19c365dcd6ee2371d6775a97dfc3c
채정훈 <chaejeonghun@chaejeonghun-ui-MacBook-Pro.local>
2018-11-14 17:26:10 +0900

    login activity

---
+++ src/org/tensorflow/yolo/view/LoginActivity.java
@@ -0,0 +1,15 @@
+package org.tensorflow.yolo.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.tensorflow.yolo.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
