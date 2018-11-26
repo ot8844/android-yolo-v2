d7c58874be8953c2176522e578fe53ed52748437
gihoon <ot8844@gmail.com>
2018-11-26 20:30:48 +0900

    classifier

--- src/org/tensorflow/yolo/view/ClassifierActivity.java
+++ src/org/tensorflow/yolo/view/ClassifierActivity.java
@@ -15,10 +15,12 @@ import android.util.TypedValue;
 
 import org.tensorflow.yolo.R;
 import org.tensorflow.yolo.TensorFlowImageRecognizer;
+import org.tensorflow.yolo.model.BoxPosition;
 import org.tensorflow.yolo.model.Recognition;
 import org.tensorflow.yolo.util.ImageUtils;
 import org.tensorflow.yolo.view.components.BorderedText;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -38,6 +40,7 @@ public class ClassifierActivity extends TextToSpeechActivity implements OnImageA
     private int previewWidth = 0;
     private int previewHeight = 0;
     private Bitmap croppedBitmap = null;
+    private Bitmap rgbFrameBitmap = null;
     private boolean computing = false;
     private Matrix frameToCropTransform;
 
@@ -76,6 +79,48 @@ public class ClassifierActivity extends TextToSpeechActivity implements OnImageA
         addCallback((final Canvas canvas) -> renderAdditionalInformation(canvas));
     }
 
+    public static List<Recognition> find(Bitmap origin) {
+        String tag = "jack";
+        int width = origin.getWidth();
+        int height = origin.getHeight();
+        int cnt = 0;
+        int cnt_2 = 0;
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int col = origin.getPixel(i, j);
+                int alpha = col & 0xFF000000;
+                int red = (col & 0x00FF0000) >> 16;
+                int green = (col & 0x0000FF00) >> 8;
+                int blue = (col & 0x000000FF);
+
+                int dist = (int) ((red - 255)*(red - 255) + (blue-0)*(blue-0) + (green-0)*(green-0));
+                if (dist < 1000){
+//                    Log.d(tag, "R: " + red + " G: " + green + " B: " + blue);
+//                    Log.d(tag, "dist : " + dist);
+//                    Log.d(tag, "X: " + i + "Y: " + j);
+                    cnt +=1;
+                    x += i;
+                    y += j;
+                }
+                if (red > 210 && blue < 40 && green < 40) {
+                    cnt_2 +=1;
+                }
+            }
+        }
+        Log.d(tag, "cnt: " + cnt);
+        Log.d(tag, "cnt2: " + cnt_2);
+        ArrayList<Recognition> res = new ArrayList<Recognition>();
+        if (cnt > 0) {
+            x = x / cnt;
+            y = y / cnt;
+            res.add(new Recognition(1, "red", 1.0f, new BoxPosition(x - 1, y - 1, 2, 2)));
+        }
+        return res;
+    }
+
     @Override
     public void onImageAvailable(final ImageReader reader) {
         Image image = null;
@@ -103,8 +148,13 @@ public class ClassifierActivity extends TextToSpeechActivity implements OnImageA
         }
 
         runInBackground(() -> {
+            String tag = "jack";
             final long startTime = SystemClock.uptimeMillis();
-            final List<Recognition> results = recognizer.recognizeImage(croppedBitmap);
+            Log.d(tag, "bitmap: " + rgbFrameBitmap.toString());
+            Log.d(tag, "width: " + rgbFrameBitmap.getHeight() + " height: " + rgbFrameBitmap.getWidth());
+            Log.d(tag, "pixel: " + rgbFrameBitmap.getPixel(1, 1));
+            //find(rgbFrameBitmap);
+            final List<Recognition> results = find(rgbFrameBitmap);//recognizer.recognizeImage(croppedBitmap);
             lastProcessingTimeMs = SystemClock.uptimeMillis() - startTime;
             overlayView.setResults(results);
             speak(results);
@@ -114,10 +164,10 @@ public class ClassifierActivity extends TextToSpeechActivity implements OnImageA
     }
 
     private void fillCroppedBitmap(final Image image) {
-            Bitmap rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Config.ARGB_8888);
+            rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Config.ARGB_8888);
             rgbFrameBitmap.setPixels(ImageUtils.convertYUVToARGB(image, previewWidth, previewHeight),
                     0, previewWidth, 0, 0, previewWidth, previewHeight);
-            new Canvas(croppedBitmap).drawBitmap(rgbFrameBitmap, frameToCropTransform, null);
+            //new Canvas(croppedBitmap).drawBitmap(rgbFrameBitmap, frameToCropTransform, null);
     }
 
     @Override
